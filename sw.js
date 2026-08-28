23b2fbee0e1e578cd6976e83106cdffac693c5f6
selyagin <mcw75jtp6w@privaterelay.appleid.com>
2026-08-28 20:44:12 +0300

    Rollback: возврат Service Worker к состоянию 7.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'densel-assistant-v11';
+const CACHE = 'densel-assistant-v6';
 const ASSETS = ['./','./index.html','./style.css','./app.js','./manifest.json','./patch-61.js','./patch-72.js'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
